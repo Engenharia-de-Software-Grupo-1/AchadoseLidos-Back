77cc4b2e9bc736b8ba337e5b365356049db99020
anarita <ana.souza.ext@synchro.com.br>
2025-02-25 22:50:54 -0300

    chore: add novas rules para a config do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,37 @@ export default [
   ...tseslint.configs.recommended,
   {
     rules: {
+      "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1, "maxBOF": 0 }],
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "indent": ["error", 1],
+      "object-curly-spacing": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-trailing-spaces": "error",
+      "eol-last": ["error", "always"],
+      "space-before-function-paren": ["error", "never"],
+      "arrow-spacing": ["error", { "before": true, "after": true }],
+
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling", "index"]
+          ],
+          "newlines-between": "always"
+        }
+      ],
+
+
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+      ],
+
+
       "no-console": "error",
     },
   },
