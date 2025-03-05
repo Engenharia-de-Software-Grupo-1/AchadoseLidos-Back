67eec9d219fbd84048bc4e52c08a14532b6525a5
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-05 12:33:15 -0300

    chore: atualiza schema do sebo e conta

--- src/models/ContaSchema.ts
+++ src/models/ContaSchema.ts
@@ -1,15 +1,8 @@
 import { z } from "zod";
 
-const TipoConta = z.enum(["SEBO", "USUARIO"]);
-const StatusConta = z.enum(["ATIVA", "EXCLUIDA"]);
-
 export const contaSchema = z.object({
   email: z.string().email(),
   senha: z.string().min(8),
-  tipo: TipoConta,
-  status: StatusConta,
 });
 
-export const contaCreateSchema = contaSchema.omit({ tipo: true, status: true });
-
-export type ContaCreateDTO = z.infer<typeof contaCreateSchema>;
+export type ContaCreateDTO = z.infer<typeof contaSchema>;
