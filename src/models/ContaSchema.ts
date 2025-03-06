6558d94cafd05e7bec7236d5a67574ce11f3b7fe
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-06 00:05:37 -0300

    chore: corrige tipo da data

--- src/models/ContaSchema.ts
+++ src/models/ContaSchema.ts
@@ -13,8 +13,8 @@ export const ContaResponseSchema = z.object({
   email: z.string(),
   tipo: TipoContaEnum,
   status: StatusContaEnum,
-  createdAt: z.string(),
-  updatedAt: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
 export type ContaCreateDTO = z.infer<typeof ContaCreateSchema>;
