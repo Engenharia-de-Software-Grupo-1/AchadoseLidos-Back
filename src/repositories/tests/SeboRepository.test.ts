235a57afb498e043ed0562a0af323ee282878bbf
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-07 23:59:51 -0300

    chore: adiciona teste para update do seboRepository

--- src/repositories/tests/SeboRepository.test.ts
+++ src/repositories/tests/SeboRepository.test.ts
@@ -1,6 +1,6 @@
 import prismaClient from "@src/lib/prismaClient";
 import { TipoConta } from "@prisma/client";
-import { SeboCreateDTO } from "@src/models/SeboSchema";
+import { SeboCreateDTO, SeboUpdateDTO } from "@src/models/SeboSchema";
 
 import { seboRepository } from "../SeboRepository";
 import { contaRepository } from "../ContaRepository";
@@ -114,5 +114,43 @@ describe("SeboRepository", () => {
     });
   });
 
-  // resta testar o método update
+  it("updates a sebo by id", async() => {
+    const seboId = 1;
+    const sebo: SeboUpdateDTO = {
+      id: seboId,
+      nome: "Example Name",
+      cpfCnpj: "123.456.789-00",
+      concordaVender: false,
+      endereco: {
+        cep: "12345-678",
+        estado: "Estado",
+        cidade: "Cidade",
+        bairro: "Bairro",
+        rua: "Rua",
+        numero: "123",
+        ehPublico: true,
+        complemento: "Complemento",
+      },
+      fotos: [{ url: "http://example.com/foto1" }],
+    };
+
+    (prismaClient.$transaction as jest.Mock).mockImplementation(async(fn) =>
+      fn({
+        sebo: { update: jest.fn() },
+        enderecoSebo: { update: jest.fn() },
+        fotoSebo: { deleteMany: jest.fn(), createMany: jest.fn() },
+      }),
+    );
+
+    (prismaClient.sebo.findUnique as jest.Mock).mockResolvedValue(sebo);
+
+    const result = await seboRepository.update(seboId, sebo);
+
+    expect(result).toEqual(sebo);
+    expect(prismaClient.$transaction).toHaveBeenCalled();
+    expect(prismaClient.sebo.findUnique).toHaveBeenCalledWith({
+      where: { id: 1 },
+      include: { endereco: true, fotos: true },
+    });
+  });
 });
