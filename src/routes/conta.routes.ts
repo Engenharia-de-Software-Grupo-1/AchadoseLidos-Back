7239a4e28dece1df69ef9090a7fb3bde3f75ab65
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-08 10:48:50 -0300

    chore: configura função async ()

--- src/routes/conta.routes.ts
+++ src/routes/conta.routes.ts
@@ -3,10 +3,10 @@ import { contaController } from '@src/controllers/ContaController';
 
 export const contaRoutes = Router();
 
-contaRoutes.get('/validar_email', async(req, res) => {
+contaRoutes.get('/validar_email', async (req, res) => {
   await contaController.validarEmail(req, res);
 });
 
-contaRoutes.delete('/:id', async(req, res) => {
+contaRoutes.delete('/:id', async (req, res) => {
   await contaController.delete(req, res);
 });
--- src/routes/sebo.routes.ts
+++ src/routes/sebo.routes.ts
@@ -3,18 +3,18 @@ import { seboController } from '@src/controllers/SeboController';
 
 export const seboRoutes = Router();
 
-seboRoutes.post('/', async(req, res) => {
+seboRoutes.post('/', async (req, res) => {
   await seboController.create(req, res);
 });
 
-seboRoutes.get('/', async(req, res) => {
+seboRoutes.get('/', async (req, res) => {
   await seboController.getAll(req, res);
 });
 
-seboRoutes.get('/:id', async(req, res) => {
+seboRoutes.get('/:id', async (req, res) => {
   await seboController.getById(req, res);
 });
 
-seboRoutes.put('/:id', async(req, res) => {
+seboRoutes.put('/:id', async (req, res) => {
   await seboController.update(req, res);
 });
