f2ceafe869f100f7ec5b6b7451e7a6ba2c37a9e9
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-15 23:05:59 -0300

    chore: remove código do jwt

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,30 +1,5 @@
-import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import bcrypt, { genSalt } from 'bcrypt';
-import { AppError } from '@src/errors/AppError';
-
-const JWT_SECRET = process.env.JWT_SECRET;
-const TOKEN_EXPIRATION = '1h';
-
-if (!JWT_SECRET) {
-  throw new Error('JWT_SECRET não definido no .env');
-}
-
-export const gerarToken = (email: string) => {
-  return jwt.sign({ email }, JWT_SECRET, { expiresIn: TOKEN_EXPIRATION });
-};
-
-export const verificarToken = (token: string) => {
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET) as { email: string };
-    return decoded.email;
-  } catch (error) {
-    if (error instanceof jwt.TokenExpiredError) {
-      throw new AppError('Token expirado', 401);
-    }
-    throw new AppError('Token inválido', 400);
-  }
-};
 
 export const gerarResetToken = () => {
   const token = crypto.randomBytes(32).toString('hex');
