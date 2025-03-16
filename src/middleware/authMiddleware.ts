e9d991c81e8bb497a653b90603c94bef4b055be7
Junior Lima <geraldo.lima.junior@ccc.ufcg.edu.br>
2025-03-16 16:49:07 -0300

    refact: structures authmiddleware better

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -7,20 +7,34 @@ const requireAuth = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.authToken;
 
   if (token) {
-    if (!process.env.JWT_SECRET) {
-      throw new AppError(ErrorMessages.serverError, 500);
-    }
+    try {
+      validaJWT(token, next);
+    } catch (e) {
+      const isAppError = e instanceof AppError;
 
-    jwt.verify(token, process.env.JWT_SECRET, (err: jwt.VerifyErrors | null, _: unknown) => {
-      if (err) {
-        res.status(401).json({ message: ErrorMessages.invalidToken });
-      } else {
-        next();
+      if (isAppError) {
+        res.status(e.statusCode).json({ message: e.message });
       }
-    });
+
+      res.status(500).json({ message: ErrorMessages.serverError });
+    }
   } else {
     res.status(401).json({ message: ErrorMessages.tokenNotProvided });
   }
+};
+
+const validaJWT = (token: string, next: NextFunction) => {
+  if (!process.env.JWT_SECRET) {
+    throw new AppError(ErrorMessages.serverError, 500);
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err: jwt.VerifyErrors | null, _: unknown) => {
+    if (err) {
+      throw new AppError(ErrorMessages.invalidToken, 401);
+    } else {
+      next();
+    }
+  });
 };
 
 export { requireAuth };
