b73c8f264d4b6f3f503938c3dbf1fad340933811
Junior Lima <geraldo.lima.junior@ccc.ufcg.edu.br>
2025-03-16 17:06:26 -0300

    test: adds tests for Conta login

--- src/services/tests/ContaService.test.ts
+++ src/services/tests/ContaService.test.ts
@@ -1,11 +1,49 @@
 import { contaRepository } from '@src/repositories/ContaRepository';
 import { StatusConta } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 import { contaService } from '../ContaService';
 
+jest.mock('bcrypt');
 jest.mock('@src/repositories/ContaRepository');
 
 describe('ContaService', () => {
+  it('should return the account if email and password are valid', async () => {
+    const mockConta = { id: 1, email: 'test@example.com', senha: 'hashedPassword' };
+
+    (contaRepository.getByEmail as jest.Mock).mockResolvedValue(mockConta);
+    (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+    const result = await contaService.login('test@example.com', 'validPassword');
+
+    expect(result).toEqual(mockConta);
+    expect(contaRepository.getByEmail).toHaveBeenCalledWith('test@example.com');
+    expect(bcrypt.compare).toHaveBeenCalledWith('validPassword', 'hashedPassword');
+  });
+
+  it('should throw an error if the email does not exist', async () => {
+    (contaRepository.getByEmail as jest.Mock).mockResolvedValue(null);
+
+    await expect(contaService.login('nonexistent@example.com', 'password')).rejects.toEqual({
+      message: 'E-mail ou senha incorretos',
+      statusCode: 401,
+    });
+    expect(contaRepository.getByEmail).toHaveBeenCalledWith('nonexistent@example.com');
+  });
+
+  it('should throw an error if the password is invalid', async () => {
+    const mockConta = { id: 1, email: 'test@example.com', senha: 'hashedPassword' };
+
+    (contaRepository.getByEmail as jest.Mock).mockResolvedValue(mockConta);
+    (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+
+    await expect(contaService.login('test@example.com', 'invalidPassword')).rejects.toEqual({
+      message: 'E-mail ou senha incorretos',
+      statusCode: 401,
+    });
+    expect(bcrypt.compare).toHaveBeenCalledWith('invalidPassword', 'hashedPassword');
+  });
+
   it('should throw an error if email already exists', async () => {
     const fixedDate = new Date('2025-03-07');
     (contaRepository.getByEmail as jest.Mock).mockResolvedValue({
