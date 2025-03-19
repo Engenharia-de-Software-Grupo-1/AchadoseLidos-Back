58395479eda21656456ab12f8451466e440d0aee
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-19 00:08:13 -0300

    feat: cria classes de erro para reuso de código

---
+++ src/errors/ErrorMessages.ts
@@ -0,0 +1,10 @@
+export enum ErrorMessages {
+  serverError = 'Erro do servidor',
+  invalidToken = 'Token inválido ou ausente',
+  expiredToken = 'Token expirado',
+  tokenNotProvided = 'Token não fornecido',
+  unauthorized = 'Não autorizado',
+  wrongEmail = 'Email incorreto',
+  wrongPassword = 'Senha incorreta',
+  noPermissionForAction = 'Acesso negado. Você não tem permissão para essa ação',
+}
