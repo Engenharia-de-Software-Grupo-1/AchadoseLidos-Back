c925852b1bc8b47a8b914fa1f8fab6967112a1e7
paulorpn <paulo.ricardo.pereira.nascimento@ccc.ufcg.edu.br>
2025-03-19 16:42:34 -0300

    test: produtoController

---
+++ src/controllers/tests/ProdutoController.test.ts
@@ -0,0 +1,92 @@
+import { Request, Response } from 'express';
+import { produtoService } from '@src/services/ProdutoService';
+
+import { produtoController } from '../ProdutoController';
+
+jest.mock('@src/services/ProdutoService');
+
+describe('ProdutoController', () => {
+  it('returns 201 and the created entity when create is successful', async () => {
+    const req = {
+      body: {},
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockResult = { someResultKey: 'someResultValue' };
+    (produtoService.create as jest.Mock).mockResolvedValueOnce(mockResult);
+
+    await produtoController.create(req, res);
+
+    expect(produtoService.create).toHaveBeenCalledWith(req.body);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith(mockResult);
+  });
+
+  it('returns 200 and all entities when getAll is successful', async () => {
+    const req = {} as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockResult = { someResultKey: 'someResultValue' };
+
+    (produtoService.getAll as jest.Mock).mockResolvedValueOnce(mockResult);
+
+    await produtoController.getAll(req, res);
+
+    expect(produtoService.getAll).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockResult);
+  });
+
+  it('returns 200 and the entity when getById is successful', async () => {
+    const req = {
+      params: {
+        id: '1',
+      },
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockResult = { someResultKey: 'someResultValue' };
+    (produtoService.getById as jest.Mock).mockResolvedValueOnce(mockResult);
+
+    await produtoController.getById(req, res);
+
+    expect(produtoService.getById).toHaveBeenCalledWith(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockResult);
+  });
+
+  it('returns 200 and the updated entity when update is successful', async () => {
+    const req = {
+      params: {
+        id: '1',
+      },
+      body: { someBodyKey: 'someBodyValue' },
+    } as unknown as Request;
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
+
+    const mockResult = { someResultKey: 'someResultValue' };
+    (produtoService.update as jest.Mock).mockResolvedValueOnce(mockResult);
+
+    await produtoController.update(req, res);
+
+    expect(produtoService.update).toHaveBeenCalledWith(1, req.body);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(mockResult);
+  });
+});
