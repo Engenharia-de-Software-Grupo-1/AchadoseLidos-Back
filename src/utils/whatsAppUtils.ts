f3fa5cf05cb26a9a394d70202ba8fa98290660d0
luledra <luisa.ledra.azevedo@ccc.ufcg.edu.br>
2025-03-29 19:55:05 -0300

    refactor: melhora mensagem do pedido

--- src/utils/whatsAppUtils.ts
+++ src/utils/whatsAppUtils.ts
@@ -4,19 +4,24 @@ export function gerarLinkWhatsApp(pedido: PedidoResponseDTO) {
   const { nome, telefone } = pedido.sebo;
 
   const produtosPedido = pedido.produtos
-    .map(
-      p =>
-        `- Nome: ${p.produto.nome}, Categoria: ${p.produto.categoria}, 
-            Quantidade: ${p.quantidade}, Preço: R$ ${p.produto.preco.toFixed(2)}`,
-    )
-    .join('\n');
+    .map(p => {
+      return `- Nome: *${p.produto.nome}*
+- Categoria: *${p.produto.categoria}*
+- Quantidade: *${p.quantidade}*
+- Preço: *R$ ${p.produto.preco.toFixed(2)}*`;
+    })
+    .join('\n\n');
 
   const mensagem = [
-    'Achados e Lidos - Confirmação de Pedido\n',
-    `Olá, ${nome}! Acabei de realizar um novo pedido pelo site.\n`,
-    'Produto(s) do Pedido:',
+    'Achados e Lidos - Confirmação de Pedido',
+    '',
+    `Olá, ${nome}! Acabei de realizar um novo pedido pelo site.`,
+    '',
+    '*Produto(s) do Pedido:*',
     produtosPedido,
-    `\n*Total: R$ ${pedido.total.toFixed(2)}*\n`,
+    '',
+    `*Total: R$ ${pedido.total.toFixed(2)}*`,
+    '',
     'Gostaria de saber mais detalhes sobre as formas de pagamento e entrega do pedido.',
   ].join('\n');
 
